8c9d6b49ec4a9779456a3e37d8e44a86eee5ee7d
heretodesign <senwelo.works@gmail.com>
2019-12-13 15:26:14 +0800

    added simple material ui form input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  FormControl,
+  Input,
+  InputAdornment,
+  IconButton,
+  InputLabel
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Send from '@material-ui/icons/Send';
 
-function App() {
+const useStyle = makeStyles((theme) => ({
+  root: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200
+  }
+}));
+
+
+const App = () => {
+  const classes = useStyle();
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setOutput(`Hello ${input}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-testid="form">
+      <h3>Material Form</h3>
+      <FormControl className={classes.root}>
+        <InputLabel htmlFor="adornment-send-title" data-testid="label">
+          Enter Name
+        </InputLabel>
+        <Input
+          id="adornment-send-title"
+          type="text"
+          value={input}
+          onChange={handleChange}
+          data-testid="nameInput"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="submit answer"
+                onClick={handleSubmit}
+                data-testid="submit"
+              >
+                <Send className={classes.iconColor} />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <p data-testid="output">{output}</p>
     </div>
   );
-}
+};
 
 export default App;
